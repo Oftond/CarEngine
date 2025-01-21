003876f2d6730c6e86a84805a956dd0359a4fad5
Oftond <vladimer.sergeev.17.02.05@gmail.com>
2025-01-21 12:04:41 +0700

    first commit

---
+++ ConsoleApp15/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+abstract class Engine
+{
+    protected float _i;
+    protected List<int> _m;
+    protected List<int> _v;
+    protected float _t;
+    protected float _hm;
+    protected float _hv;
+    protected float _c;
+
+    public abstract float EngineTemp {  get; }
+    public abstract float MaxTemp { get; }
+    public abstract bool EngineStatus { get; }
+
+    public abstract void Start(float TAir);
+    public abstract void Stop();
+}
---
+++ ConsoleApp15/ICE.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+class ICE : Engine
+{
+    private float _curTemp = 0;
+    private bool _isWorking;
+    public override float EngineTemp => _curTemp;
+
+    public override float MaxTemp => _t;
+
+    public override bool EngineStatus => _isWorking;
+
+    public ICE(float I, List<int> M, List<int> V, float HM, float HV, float C)
+    {
+        _i = I;
+        _m = M;
+        _v = V;
+        _hm = HM;
+        _hv = HV;
+        _c = C;
+    }
+
+    public override void Start(float TAir)
+    {
+        float a, Vh, Vc, M, V;
+        for (int i = 0; i < _m.Count; i++)
+        {
+            M = _m[i];
+            V = _v[i];
+            while(V < _v[i + 1])
+            {
+                a = M * _i;
+                Vh = M * _hm + V * V * _hv;
+                Vc = _c * (TAir * _curTemp);
+
+                _curTemp += Vh - Vc;
+                V += a;
+                M = (V - _v[i]) * (_m[i + 1] - _m[i]) / (_v[i + 1] - _v[i] + _m[i]);
+
+                if (!_isWorking)
+                    return;
+            }
+        }
+    }
+
+    public override void Stop() => _isWorking = false;
+}
